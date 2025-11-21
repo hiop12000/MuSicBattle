dc910779559f654a1023af5d3c4e55a06d34b4fa
hiop12000 <piterhiop@gmail.com>
2025-11-21 20:44:54 +0300

    update3

--- map_rate.py
+++ map_rate.py
@@ -8,12 +8,13 @@ with open("data_/MusicMaps.json") as file:
     table = dict()
     for map in f:
         table[map["title"]] = map["visits_total"]
-    sorted_table_by_visits = sorted(table.items(), key=lambda x: x[1])
-    #sorted_table_by_visits = sorted(table.items(), key=lambda x: x[1], reverse = True)
+    sorted_table_by_visits = sorted(table.items(), key=lambda x: x[1], reverse = True)
     sorted_table = dict(sorted_table_by_visits)
+    keys = list(sorted_table.keys())[:10]
+    top_table = {key: sorted_table[key] for key in keys}
     fig, ax = plt.subplots()
     ax.set_title("Самые посещаемые карты")
-    titles = sorted_table.keys()
-    values = sorted_table.values()
+    titles = top_table.keys()
+    values = top_table.values()
     ax.barh(titles, values)
     plt.show()
--- user_activity_rate.py
+++ user_activity_rate.py
@@ -3,17 +3,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 
-with open("data_/MusicMaps.json") as file:
+with open("data_/users.json") as file:
     f = json.load(file)
     table = dict()
-    for map in f:
-        table[map["title"]] = map["visits_total"]
-    sorted_table_by_visits = sorted(table.items(), key=lambda x: x[1])
-    #sorted_table_by_visits = sorted(table.items(), key=lambda x: x[1], reverse = True)
-    sorted_table = dict(sorted_table_by_visits)
+    print(f)
+    for user in f:
+        table[user["username"]] = user["stats"]["total_maps_completed"]
+    sorted_table_by_amount = sorted(table.items(), key=lambda x: x[1], reverse = True)
+    sorted_table = dict(sorted_table_by_amount)
+    keys = list(sorted_table.keys())[:5]
+    top_table = {key: sorted_table[key] for key in keys}
     fig, ax = plt.subplots()
-    ax.set_title("Самые посещаемые карты")
-    titles = sorted_table.keys()
-    values = sorted_table.values()
-    ax.barh(titles, values)
+    ax.set_title("Топ игроков по количеству пройденных карт")
+    titles = top_table.keys()
+    values = top_table.values()
+    ax.scatter(titles, values)
     plt.show()
+
+
+#Топ активности пользователей за последнюю неделю
+
